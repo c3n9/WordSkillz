0c94b341ca95e7cd05e2b0544f5fb1b766420c7c
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-01-07 14:02:18 +0300

    add frame method

--- WordSkillz/Pages/AddWordsPage.xaml.cs
+++ WordSkillz/Pages/AddWordsPage.xaml.cs
@@ -5,10 +5,42 @@ public partial class AddWordsPage : ContentPage
 	public AddWordsPage()
 	{
 		InitializeComponent();
+        NewWord();
 	}
+
+    private void NewWord()
+    {
+        var frame = new Frame
+        {
+            CornerRadius = 20,
+            BorderColor = Color.FromHex("#512BD4"),
+            Margin = new Thickness(10),
+            Content = new VerticalStackLayout
+            {
+                Children =
+                {
+                    new Entry { WidthRequest = 200 },
+                    new Entry { WidthRequest = 200 },
+                    new Button
+                    {
+                        Text = "Save",
+                        HorizontalOptions = LayoutOptions.EndAndExpand,
+                        WidthRequest = 200,
+                        CornerRadius = 10
+                    }
+                }
+            }
+        };
+        VSLWords.Children.Add(frame);
+    }
 
     private async void BBack_Clicked(object sender, EventArgs e)
     {
 		await Navigation.PopModalAsync();
+    }
+
+    private void BAddWord_Clicked(object sender, EventArgs e)
+    {
+        NewWord();
     }
 }
