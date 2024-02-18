d973103206ed6494f92213c2c73847a66828d4bd
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-02-18 15:28:10 +0300

    fixed a crash when there are 0 words in the category

--- WordSkillz/Popup/MiniGamesPopup.xaml.cs
+++ WordSkillz/Popup/MiniGamesPopup.xaml.cs
@@ -1,15 +1,17 @@
+using System.Collections.ObjectModel;
 using WordSkillz.Models;
 using WordSkillz.Pages.MiniGamePages;
+using WordSkillz.Tools;
 
 namespace WordSkillz.Popup;
 
 public partial class MiniGamesPopup : ContentView
 {
-	public Category Category { get; set; }
-	public MiniGamesPopup()
-	{
-		InitializeComponent();
-	}
+    public Category Category { get; set; }
+    public MiniGamesPopup()
+    {
+        InitializeComponent();
+    }
 
     private async void BPlayViewWords_Clicked(object sender, EventArgs e)
     {
@@ -17,7 +19,9 @@ public partial class MiniGamesPopup : ContentView
         {
             parentPopup.Close();
         }
-        await App.Current.MainPage.Navigation.PushAsync(new WordCardsPage(Category));
+        var words = new ObservableCollection<Word>(DataManager.AllWords.Where(x => x.CategoryId == Category.Id));
+        if (words.Count != 0)
+            await App.Current.MainPage.Navigation.PushAsync(new WordCardsPage(Category));
     }
 
     private void BPlayMatchWords_Clicked(object sender, EventArgs e)
