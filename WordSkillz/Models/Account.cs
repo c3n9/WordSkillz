ab95c4ffe94e49ece01b7aaabc926902892494a0
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-02-27 19:42:50 +0300

    added account class

---
+++ WordSkillz/Models/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WordSkillz.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Photo { get; set; }
+        [JsonIgnore]
+        private ImageSource _photoImageSource;
+        [JsonIgnore]
+        public ImageSource PhotoImageSource
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Photo))
+                {
+                    _photoImageSource = ImageSource.FromStream(() => new MemoryStream(Convert.FromBase64String(Photo)));
+                }
+                return _photoImageSource;
+            }
+            set
+            {
+                _photoImageSource = value;
+            }
+        }
+    }
+}
+
