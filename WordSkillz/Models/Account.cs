3a280ff5ca366a078e37d4977d8187ae85b1a3f4
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-02-28 21:03:37 +0300

    add properties for account class

--- WordSkillz/Models/Account.cs
+++ WordSkillz/Models/Account.cs
@@ -14,6 +14,11 @@ namespace WordSkillz.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string Photo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         [JsonIgnore]
         private ImageSource _photoImageSource;
         [JsonIgnore]
